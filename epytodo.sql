608a3e3715ce894b08b2ac58efd643d442254c01
ElouanR <elouan.rigomont@epitech.eu>
2023-05-15 11:33:57 +0200

    [+] add mysql + epytodo.sql

---
+++ epytodo.sql
@@ -0,0 +1,21 @@
+CREATE TABLE user (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    firstname VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE todo (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    due_time DATETIME NOT NULL,
+    status ENUM('not started', 'todo', 'in progress', 'done') DEFAULT 'not started',
+    user_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);

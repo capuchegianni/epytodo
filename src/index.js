422cc41fa101cd530b57706c12cb06c3c7a49de3
Gianni Henriques <gianni.henriques@epitech.eu>
2023-05-16 16:31:10 +0200

    [-] remove bodyparser

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 const express = require('express')
-const dotenv = require('dotenv').config( {path: '../.env'} );
-const bodyParser = require('body-parser');
+require('dotenv').config( {path: '../.env'} );
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 require('./routes/auth/auth.js')(app);
 require('./routes/user/user.js')(app);
 require('./routes/todos/todos.js')(app);

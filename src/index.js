4f2177b2acd821f578ea7223d953e785ed316fab
ElouanR <elouan.rigomont@epitech.eu>
2023-05-16 14:37:33 +0200

    [~] change version of modules and update/connect db

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ const dotenv = require('dotenv').config( {path: '../.env'} );
 const bodyParser = require('body-parser');
 const app = express()
 const port = process.env.PORT || 3000;
+const db = require('./config/db.js');
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 require('./routes/auth/auth.js')(app);
 require('./routes/user/user.js')(app);
 require('./routes/todos/todos.js')(app);
+
+app.get('/', (req, res) => {
+  res.send('Server running.\n');
+});
 
 app.listen(port, () => {
   console.log(`To do list running on port ${port}`);

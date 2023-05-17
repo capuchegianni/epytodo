d65fad6acba26c257e13b684ef2d24eb7742f06f
ElouanR <elouan.rigomont@epitech.eu>
2023-05-17 16:37:18 +0200

    [-] remove double function TodoIdFromToken

--- src/routes/todos/todos.js
+++ src/routes/todos/todos.js
@@ -34,30 +34,6 @@ function TodoIdFromToken(authHeader, res) {
     });
 }
 
-function TodoIdFromToken(authHeader, res) {
-    if (!authHeader) {
-        res.status(401).send(JSON.stringify({ msg: 'No token, authorization denied' }, null, 2) + '\n');
-        return;
-    }
-    return new Promise((resolve) => {
-        try {
-            const token = authHeader.split(' ')[1];
-            jwt.verify(token, process.env.SECRET);
-            const userEmail = jwt.decode(token, process.env.SECRET);
-            const query = `SELECT t.id AS todo_id FROM user u JOIN todo t ON u.id = t.user_id WHERE u.email = ?`;
-            db.query(query, [userEmail], (err, result) => {
-                if (err || result.length === 0) {
-                    res.status(400).send(JSON.stringify({ msg: 'Not found' }, null, 2) + '\n');
-                } else {
-                    resolve(result[0].id);
-                }
-            });
-        } catch (err) {
-            res.status(401).send(JSON.stringify({ msg: 'Token is not valid' }, null, 2) + '\n');
-        }
-    });
-}
-
 module.exports = function(app) {
     app.get('/todos', async (req, res) => {
         const todoId = await TodoIdFromToken(req.headers.authorization, res);
